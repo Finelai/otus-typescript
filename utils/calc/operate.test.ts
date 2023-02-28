24c477da065ea93f964721fce8dd2708957299d0
Finelai <5221032+Finelai@users.noreply.github.com>
2023-02-28 17:32:14 +0300

    homework1_calc-finish

--- utils/calc/operate.test.ts
+++ utils/calc/operate.test.ts
@@ -5,4 +5,10 @@ describe('test calculator main operations', () => {
   test('sum of two', () => {
     expect(operate('2+3')).toBe('5');
   });
+  test('calc big numbers', () => {
+    expect(operate('272+2*4')).toBe('280');
+  });
+  test('minus operations', () => {
+    expect(operate('4*3-112')).toBe('-100');
+  });
 });
