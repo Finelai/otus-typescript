0121c117336d356f0cb4914e493ca7b1b350ba7e
Finelai <5221032+Finelai@users.noreply.github.com>
2023-02-28 15:30:11 +0300

    homework1_add-operate

--- utils/calc/operate.ts
+++ utils/calc/operate.ts
@@ -3,10 +3,31 @@
  */
 
 export const operate = (calcValue: string): string => {
-  const supportedOperations = ['+', '-', '*', '/'];
-  const trimmedCalcVal = calcValue.replace(/ /g, '');
+  const supportedOperations: Array<string> = ['*', '/', '+', '-'];
+  let trimmedCalcVal: string = calcValue.trim();
+
+  function doOperate(operator: string, n1: number, n2: number) {
+    switch(operator) {
+      case '*': return n1 * n2;
+      case '/': return n1 / n2;
+      case '-': return n1 - n2;
+      case '+': return n1 + n2;
+    }
+
+    return 0;
+  }
 
   if (trimmedCalcVal !== '') {
+    for (let i = 0; i < supportedOperations.length; i++) {
+      for (let v = 0; v < trimmedCalcVal.length; v++) {
+        if (supportedOperations[i] === trimmedCalcVal[v]) {
+          const operateToChange: string = trimmedCalcVal.slice(trimmedCalcVal[v - 1], trimmedCalcVal[v + 1]);
+          const operateResult: number = doOperate(supportedOperations[i], parseInt(trimmedCalcVal[v - 1]), parseInt(trimmedCalcVal[v + 1]));
+          trimmedCalcVal = trimmedCalcVal.replace(operateToChange, operateResult.toString());
+        }
+      }
+    }
+
     return trimmedCalcVal;
   }
 
